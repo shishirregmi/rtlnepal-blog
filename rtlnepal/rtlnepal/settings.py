f0a283f9671e77a6a3bc39ffd421de41cff0711f
shishir <shishir.regmi56@gmail.com>
2020-11-08 21:13:33 +0545

    made changes by adding views in url , created model,form and view with login template

--- rtlnepal/rtlnepal/settings.py
+++ rtlnepal/rtlnepal/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rtlnepal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+LOGIN_REDIRECT_URL = '/'
--- rtlnepal/rtlnepal/urls.py
+++ rtlnepal/rtlnepal/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import url,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'',include'(blogs.url'))
+    url(r'^accounts/login/$',views.login,name='login')
+    url(r'^accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'})
 ]
