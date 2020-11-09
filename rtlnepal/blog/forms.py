25fb3c6619942d2d2d95a80615ba4d5cf913e11a
shishir <shishir.regmi56@gmail.com>
2020-11-09 22:41:48 +0545

    fixing all errors and no texteditor

--- rtlnepal/blog/forms.py
+++ rtlnepal/blog/forms.py
@@ -5,7 +5,7 @@ class PostForm(forms.ModelForm):
 
     class Meta():
         model = Post
-        fields = ('author','title','text')
+        fields = ('author','title','image','text')
 
         widgets = {
             'title':forms.TextInput(attrs={'class':'form-control'}),
