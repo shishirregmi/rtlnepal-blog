683eaa56c8b7e72c50cdbc7b736b1b596d56b406
Shishir Regmi <rshishir21@gmail.com>
2020-11-10 16:11:51 +0545

    Update views.py
    commnet--->comment

--- rtlnepal/blog/views.py
+++ rtlnepal/blog/views.py
@@ -64,7 +64,7 @@ def add_comment_to_post(request,pk):
     if request.method == 'POST':
         form =CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save(comment=False)
+            comment = form.save(commit=False)
             comment.post = post
             comment.save()
             return redirect('post_detail',pk=post.pk)
